224db98a212c6ea4df2c56c7aab242758f90ad30
AlejandroPerezGarcia <alejandro8001@gmail.com>
2020-08-16 20:28:43 -0400

    Spiner funcionando

--- app/src/main/java/cl/desafiolatam/Cyclobike/MainActivity.kt
+++ app/src/main/java/cl/desafiolatam/Cyclobike/MainActivity.kt
@@ -4,13 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
+import android.widget.AdapterView.OnItemLongClickListener
+import android.widget.ArrayAdapter
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , AdapterView.OnItemSelectedListener,
+    OnItemLongClickListener{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listof = listOf( "Las Condes","La Reina","Ñuñoa","Macul","Providencia")
+
+        val adapter = ArrayAdapter(this,android.R.layout.simple_spinner_item,listof)
+        FiltroComuna.adapter = adapter
+
 
 
     }
@@ -27,6 +36,20 @@ class MainActivity : AppCompatActivity() {
     */
     fun LasCondes(view: View) {
         cyclo_recycler.adapter = CycloAdapter(SetupCiclovias().condes())
+    }
+
+
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onItemLongClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long): Boolean {
+        TODO("Not yet implemented")
     }
 
 }
